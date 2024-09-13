5fadddd12de3d50408fd1da9f150ec78fde4db6a
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-09-13 09:47:58 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/nav/Navbar'
 import Inicio from './components/pages/inicio'
+import Footer from './components/footer/Footer'
 
 function App() {
 
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const footer = () => {
+  return (
+    <div>
+      <footer className='text-white py-4 ng black'>
+        <div className='container'>
+          <nav className=''>
+            <Link to='/' className='col-12 col-md-3 d-flex aling-items-center justify-content-center'>
+            </Link>
+            <ul className='col-12 col-md-3 list-unstyled'>
+              <li className='font-weight-bold md-2'>Creado por Isaac</li>
+              <li className='text-center'>https://github.com/AisaacVM/evamar</li>
+            </ul>
+            <ul className='col-12 col-md-3 list-unstyled'>
+              <li className='font-weight-bold md-2'>Creado por Gustavo</li>
+              <li className='text-center'>https://github.com/GustavoAR08/evamar</li>
+            </ul>
+            <ul>
+              <li className='font-weight-bold md-2'>Creado por Julian</li>
+              <li className='text-center'>https://github.com/fernandez2006/front2</li>
+            </ul>
+          </nav>
+        </div>
+
+      </footer>
+
+  </div>
+  )
+}
+
+export default footer
